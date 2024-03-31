028845f9294dfc664668c0fac9fa52cd712b2ed9
Afash <afashseven@gmail.com>
2024-03-31 05:03:01 +0330

    initial

--- members/admin.py
+++ members/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Member
 
-admin.site.register(Member)
+# admin.site.register(Member)
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", "join_date")
+    
+admin.site.register(Member, MemberAdmin)
