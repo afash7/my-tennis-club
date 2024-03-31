899634d0b01299c7a6368f22c6b2b5a9a0529fb9
Afash <afashseven@gmail.com>
2024-03-31 05:38:36 +0330

    use django variables

--- members/views.py
+++ members/views.py
@@ -30,8 +30,11 @@ def main(request):
 
 
 def testing(request):
+    mymembers = Member.objects.all().values()
     template = loader.get_template('template.html')
     context = {
-        'fruits': ['Apple', 'Bnana', 'Cherry']
+        'fruits': ['Apple', 'Bnana', 'Cherry'],
+        'firstname': 'Linus',
+        'mymembers': mymembers,
     }
     return HttpResponse(template.render(context, request))
