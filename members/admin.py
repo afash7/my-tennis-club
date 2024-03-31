df47b66ceb0f1e480602df6172fc24b2fc89d662
Afash <afashseven@gmail.com>
2024-03-31 05:03:46 +0330

    set list display

--- members/admin.py
+++ members/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Member
 
-# admin.site.register(Member)
-
 class MemberAdmin(admin.ModelAdmin):
     list_display = ("firstname", "lastname", "join_date")
     
